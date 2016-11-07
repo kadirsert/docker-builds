2bc29f714fd1fdff0cf0738aa65a160170c4de9a
Kadir Sert <kadirsert80@gmail.com>
2016-11-07 15:12:29 +0300

    some changes for cupsd.conf

--- centos7_cups/config.sh
+++ centos7_cups/config.sh
@@ -8,6 +8,6 @@ sed -i 's/SystemGroup sys root/SystemGroup sys root cupsadmins/' /etc/cups/cups-
 
 sed -i 's/Listen localhost:631/Listen 0.0.0.0:631/' /etc/cups/cupsd.conf
 sed -i 's/Browsing On/Browsing Off/' /etc/cups/cupsd.conf
-sed -i '/Order allow,deny/a Allow from 192.168.0.0\/16/' /etc/cups/cupsd.conf
-sed -i '/Order allow,deny/a Allow from 172.16.0.0\/12/' /etc/cups/cupsd.conf
-sed -i '/Order allow,deny/a Allow from 10.0.0.0\/8/' /etc/cups/cupsd.conf
+sed -i '/Order allow,deny/a   Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
+sed -i '/Order allow,deny/a   Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
+sed -i '/Order allow,deny/a   Allow from 10.0.0.0\/8' /etc/cups/cupsd.conf
