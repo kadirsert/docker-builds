6026530e13c4661c1946a740ddf85c3c97ed1e03
Kadir SERT (GM Bilgi Sistemleri) <gmbim22@ykdockertest01.yurticikargo.local>
2016-11-15 16:53:46 +0300

    centos6_cups added

---
+++ centos6_cups/config.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+groupadd cupsadmins
+useradd -g cupsadmins -c 'CUPS administration account' cupsadmin
+echo "cupsadmin:Password" | chpasswd
+
+#sed -i 's/LogLevel warn/LogLevel debug/' /etc/cups/cupsd.conf
+
+sed -i 's/SystemGroup sys root/SystemGroup sys root cupsadmins/' /etc/cups/cupsd.conf
+sed -i 's/Listen localhost:631/Listen *:631/' /etc/cups/cupsd.conf
+sed -i 's/Browsing On/Browsing Off/' /etc/cups/cupsd.conf
+
+sed -i '/DefaultAuthType Basic/a ServerAlias *' /etc/cups/cupsd.conf
+
+sed -i '/  Order allow,deny/a   Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
+sed -i '/  Order allow,deny/a   Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
+sed -i '/  Order allow,deny/a   Allow from 10.0.0.0\/8' /etc/cups/cupsd.conf
+
