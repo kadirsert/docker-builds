71e8f56f9dc773b2cb7c349b9a1efe9f8e0a0f6f
Kadir Sert <kadirsert80@gmail.com>
2016-11-16 13:22:08 +0300

    centos6_cups config.sh changed

--- centos6_cups/config.sh
+++ centos6_cups/config.sh
@@ -12,8 +12,9 @@ sed -i 's/Browsing On/Browsing Off/' /etc/cups/cupsd.conf
 
 sed -i '/DefaultAuthType Basic/a DefaultEncryption Never' /etc/cups/cupsd.conf
 sed -i '/DefaultEncryption Never/a ServerAlias *' /etc/cups/cupsd.conf
+sed -i '/<Location \/admin>/a \ \ AuthType Default\n  Require user @SYSTEM' etc/cups/cupsd.conf
 
-sed -i '/  Order allow,deny/a   Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
-sed -i '/  Order allow,deny/a   Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
-sed -i '/  Order allow,deny/a   Allow from 10.0.0.0\/8' /etc/cups/cupsd.conf
+sed -i '/  Order allow,deny/a \ \ Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
+sed -i '/  Order allow,deny/a \ \ Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
+sed -i '/  Order allow,deny/a \ \ Allow from 10.0.0.0\/8' /etc/cups/cupsd.conf
 
