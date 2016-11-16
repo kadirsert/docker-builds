bc0144bd542579c989473310d6df03a345248a0b
Kadir Sert <kadirsert80@gmail.com>
2016-11-16 11:39:22 +0300

    centos6_cups config.sh changed

--- centos6_cups/config.sh
+++ centos6_cups/config.sh
@@ -10,7 +10,8 @@ sed -i 's/SystemGroup sys root/SystemGroup sys root cupsadmins/' /etc/cups/cupsd
 sed -i 's/Listen localhost:631/Listen *:631/' /etc/cups/cupsd.conf
 sed -i 's/Browsing On/Browsing Off/' /etc/cups/cupsd.conf
 
-sed -i '/DefaultAuthType Basic/a ServerAlias *' /etc/cups/cupsd.conf
+sed -i '/DefaultAuthType Basic/a DefaultEncryption Never' /etc/cups/cupsd.conf
+sed -i '/DefaultEncryption Never/a ServerAlias *' /etc/cups/cupsd.conf
 
 sed -i '/  Order allow,deny/a   Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
 sed -i '/  Order allow,deny/a   Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
