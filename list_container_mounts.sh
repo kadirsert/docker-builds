782829f2f79b733c94362b12944fd21336e74086
Kadir Sert <kadirsert80@gmail.com>
2016-11-25 16:34:34 +0300

    list_container_mounts.sh

--- list_container_mounts.sh
+++ list_container_mounts.sh
@@ -5,7 +5,7 @@ IFS='
 for container_name in $(docker ps | grep -v "IMAGE" | awk 'NF>1{print $NF}')
 do
   echo "${container_name}:"
-  mounts=$(docker inspect --format='{{range .Mounts}}{{ .Destination }} {{ .Source }} {{end}}' ${container_name})
+  mounts=$(docker inspect --format='{{range .Mounts}}{{ .Destination }} {{ .Source }}:{{end}}' ${container_name})
   read -r -a mounts_arr <<< "${mounts}"
   for ind in "${!mounts_arr[@]}"
   do
