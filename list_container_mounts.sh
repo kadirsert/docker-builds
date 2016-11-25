b4b9306cb8968a5683b1df538576cdfefaa7d631
Kadir Sert <kadirsert80@gmail.com>
2016-11-25 16:17:35 +0300

    list_container_mounts.sh

--- list_container_mounts.sh
+++ list_container_mounts.sh
@@ -5,8 +5,8 @@ do
   echo "${container_name}:"
   mounts=$(docker inspect --format='{{range .Mounts}}{{ .Destination }} {{ .Source }}||{{end}}' ${container_name})
   IFS='||' read -r -a mounts_arr <<< "${mounts}"
-  for mount in "${mounts_arr[@]}"
+  for ind in "${!mounts_arr[@]}"
   do
-    echo ${mount}
+    echo $ind ${mounts_arr[ind]}
   done
 done
