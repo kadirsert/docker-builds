2f7434d9e7936a25fd7831acc518ed78de74740b
Kadir Sert <kadirsert80@gmail.com>
2016-11-28 16:16:12 +0300

    centos7_cups

--- centos7_cups/config.sh
+++ centos7_cups/config.sh
@@ -5,10 +5,10 @@ useradd -g cupsadmins -c 'CUPS administration account' cupsadmin
 echo "cupsadmin:Password" | chpasswd
 
 sed -i 's/SystemGroup sys root/SystemGroup sys root cupsadmins/' /etc/cups/cups-files.conf
-sed -i 's/#DocumentRoot \/usr\/share\/cups\/www/DocumentRoot \/usr\/share\/cups\/www/' /etc/cups/cups-files.conf
 
 sed -i 's/Listen localhost:631/Listen 0.0.0.0:631/' /etc/cups/cupsd.conf
 sed -i 's/Browsing On/Browsing Off/' /etc/cups/cupsd.conf
+
 sed -i '/Order allow,deny/a   Allow from 192.168.0.0\/16' /etc/cups/cupsd.conf
 sed -i '/Order allow,deny/a   Allow from 172.16.0.0\/12' /etc/cups/cupsd.conf
 sed -i '/Order allow,deny/a   Allow from 10.0.0.0\/8' /etc/cups/cupsd.conf
